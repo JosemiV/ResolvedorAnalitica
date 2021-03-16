1b3a8a465a360c8ced0d17adda43498338eda704
JosemiV <josemi.vahu@gmail.com>
2021-03-16 17:33:43 -0500

    Primeros objetos y funciones analitica

---
+++ ObjetosAnalitica.py
@@ -0,0 +1,20 @@
+class Recta:
+    paso = 0; pend = 0;
+
+#libreria math
+def ang2R:
+
+
+def tan2R:
+
+#devuelva del menor angulo
+def cos2R(R1, R2):
+    m1 = R1.pend; m2 = R2.pend; ps1 = R1.paso;
+    #+ * / - sq() ^2;
+    return (1 + m1*m2) / (sq(1+m1^2) * sq(1+m2^2));
+
+def disptR:
+
+def dis2R:
+
+
